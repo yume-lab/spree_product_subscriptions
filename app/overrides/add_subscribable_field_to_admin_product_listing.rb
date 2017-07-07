f41a3df5b68cd3f44545317a35ead00fe78dfd30
manishrawat0212 <manishrawat0212@gmail.com>
2017-07-07 22:59:42 +0530

    deface override fix #52

--- app/overrides/add_subscribable_field_to_admin_product_listing.rb
+++ app/overrides/add_subscribable_field_to_admin_product_listing.rb
@@ -1,13 +1,13 @@
 Deface::Override.new(
   virtual_path: "spree/admin/products/index",
   name: "add_subscribable_header_to_products_listing",
-  insert_before: "[data-hook='admin_products_index_headers'] .text-center",
+  insert_before: "[data-hook='admin_products_index_headers'] [data-hook='admin_products_index_header_actions']",
   partial: "spree/admin/products/subscribable_listing_header"
 )
 
 Deface::Override.new(
   virtual_path: "spree/admin/products/index",
   name: "add_subscribable_content_to_products_listing",
-  insert_before: "[data-hook='admin_products_index_rows'] .text-center",
+  insert_before: "[data-hook='admin_products_index_rows'] [data-hook='admin_products_index_row_actions']",
   partial: "spree/admin/products/subscribable_listing_content"
 )
